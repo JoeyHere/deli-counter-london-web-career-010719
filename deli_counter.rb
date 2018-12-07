e12f4f9c75eaa9a57fafcf78c5654c490559bee1
Joseph O'Dell <josephodell@outlook.com>
2018-12-07 16:09:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+
+def line(array)
+  string = "The line is currently: "
+ array.each_with_index do |x, index|
+   string = string + "#{index+1}. #{x} "
+ end
+
+ if array.size < 1
+    puts "The line is currently empty."
+ else
+   string.slice!(-1)
+ puts string
+ end
+ end
